73d0090e0d86866fb2754717773af0a00d508c46
Bas Nieuwenhuizen <bas@basnieuwenhuizen.nl>
2018-05-29 02:52:01 +0200

    Add processing of exclusion lists.

--- runtests.sh
+++ runtests.sh
@@ -1,21 +1,31 @@
+BUILDERNAME=tonga
+MESA_ID=t
+
+FAMILY=$BUILDERNAME
+
 [ -d artifacts ] && rm -R artifacts
 
 set -e
 
 scp -o StrictHostKeyChecking=no stashed-file-writer@ci.basnieuwenhuizen.nl:/srv/stashed-files/vulkan-master.tar.xz ./vulkan.tar.xz
-scp -o StrictHostKeyChecking=no stashed-file-writer@ci.basnieuwenhuizen.nl:/srv/stashed-files/mesa-t.tar.xz mesa.tar.xz
+scp -o StrictHostKeyChecking=no stashed-file-writer@ci.basnieuwenhuizen.nl:/srv/stashed-files/mesa-${MESA_ID}.tar.xz mesa.tar.xz
 
 
 tar xvf vulkan.tar.xz
 tar xvf mesa.tar.xz
 
-
-
-sed -i "s#\".*artifacts#\"$PWD\/artifacts#g"  $PWD/artifacts/share/vulkan/icd.d/radeon_icd.x86_64.json
+sed -i "s#\"library_path\": \".*artifacts#\"library_path\": \"$PWD\/artifacts#g"  $PWD/artifacts/share/vulkan/icd.d/radeon_icd.x86_64.json
 export VK_ICD_FILENAMES=$PWD/artifacts/share/vulkan/icd.d/radeon_icd.x86_64.json
 export LD_LIBRARY_PATH=$PWD/artifacts/lib
 
-artifacts/bin/vulkan-cts-runner --deqp "$PWD/artifacts/vulkan-cts/external/vulkancts/modules/vulkan/deqp-vk" --caselist  "$PWD/artifacts/mustpass.txt" --output results.csv
 
+cp artifacts/mustpass.txt mustpass.txt
+[ -f worker_scripts/exclusions/${FAMILY?}/unstable.txt ] && (grep -vFf worker_scripts/exclusions/${FAMILY?}/unstable.txt artifacts/mustpass.txt > mustpass.txt) || true
 
+artifacts/bin/vulkan-cts-runner --deqp "$PWD/artifacts/vulkan-cts/external/vulkancts/modules/vulkan/deqp-vk" --caselist  "$PWD/mustpass.txt" --output results.csv
 
+cp results.csv results_filtered.csv
+[ -f worker_scripts/exclusions/${FAMILY?}/failing.txt ] && (grep -vFf worker_scripts/exclusions/${FAMILY?}/failing.txt results.csv > results_filtered.csv) || true
+
+echo "Failures:"
+! grep Fail\\\|Crash results_filtered.csv 
